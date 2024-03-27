e1c307e2e186742c266a295e740ba5f99d8b1be9
James Randall <james@accidentalfish.com>
2024-03-27 08:37:35 +0000

    Zoning now deals with different tool types

--- src/tools/applyTool.ts
+++ src/tools/applyTool.ts
@@ -2,17 +2,28 @@ import { Game, Tool } from "../model/game"
 import { rectFromRange } from "../utilities"
 import { ZoneEnum } from "../model/Landscape"
 import { updateRendererTileInfo } from "../resources/landscape"
+import { isZoningTool, zoneForTool } from "./utilities"
 
 export function applyTool(gl: WebGL2RenderingContext, game: Game) {
-  if (game.gui.currentTool === Tool.DenseResidential && game.gui.selection !== null) {
-    const r = rectFromRange(game.gui.selection)
-    for (let x = r.left; x <= r.right; x++) {
-      for (let y = r.top; y <= r.bottom; y++) {
-        if (game.landscape.tileInfo[y][x].isFlat) {
-          game.landscape.tileInfo[y][x].zone = ZoneEnum.DenseResidential
+  if (isZoningTool(game.gui.currentTool) && game.gui.selection !== null) {
+    applyZoning(gl, game)
+  }
+}
+
+function applyZoning(gl: WebGL2RenderingContext, game: Game) {
+  const newZone = zoneForTool(game.gui.currentTool)
+  const r = rectFromRange(game.gui.selection!)
+
+  for (let x = r.left; x <= r.right; x++) {
+    for (let y = r.top; y <= r.bottom; y++) {
+      if (game.landscape.tileInfo[y][x].isFlat) {
+        if (game.landscape.tileInfo[y][x].zone !== newZone) {
+          // TODO: delete buildings etc.
         }
+        game.landscape.tileInfo[y][x].zone = newZone
       }
     }
-    updateRendererTileInfo(gl, game.landscape, game.gui.selection)
   }
+
+  updateRendererTileInfo(gl, game.landscape, game.gui.selection!)
 }
--- src/tools/utilities.ts
+++ src/tools/utilities.ts
@@ -1,4 +1,5 @@
 import { Tool, ToolSelectionMode } from "../model/game"
+import { ZoneEnum } from "../model/Landscape"
 
 export function applyToolClearsSelection(tool: Tool) {
   switch (tool) {
@@ -42,5 +43,40 @@ export function toolSelectionMode(tool: Tool) {
       return ToolSelectionMode.Single // we actually want this to be a direction locked "1 unit wide" range
     default:
       return ToolSelectionMode.None
+  }
+}
+
+export function isZoningTool(tool: Tool) {
+  switch (tool) {
+    case Tool.LightCommercial:
+    case Tool.LightIndustrial:
+    case Tool.LightResidential:
+    case Tool.DenseResidential:
+    case Tool.DenseIndustrial:
+    case Tool.DenseCommercial:
+    case Tool.Dezone:
+      return true
+    default:
+      return false
+  }
+}
+
+export function zoneForTool(tool: Tool) {
+  switch (tool) {
+    case Tool.LightCommercial:
+      return ZoneEnum.LightCommercial
+    case Tool.LightIndustrial:
+      return ZoneEnum.LightIndustrial
+    case Tool.LightResidential:
+      return ZoneEnum.LightResidential
+    case Tool.DenseResidential:
+      return ZoneEnum.DenseResidential
+    case Tool.DenseIndustrial:
+      return ZoneEnum.DenseIndustrial
+    case Tool.DenseCommercial:
+      return ZoneEnum.DenseCommercial
+    default:
+    case Tool.Dezone:
+      return ZoneEnum.None
   }
 }
